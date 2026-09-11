cbac8936828062c282cee9050d510866a1b0ad7e
VictorgGonzalezMorales-1 <victor.gonzalez@alumnos.ucn.cl>
2026-09-10 21:26:46 -0300

    Lectura de Archivo
    - Subida del txt pacientes
    - Creación del método para leer el archivo

--- main.cpp
+++ main.cpp
@@ -1,13 +1,39 @@
 
 /*
  * Taller 1 Estructura de Datos
- * Integrantes: Victor Manuel Gonzalez Morales
+ * Integrante: Victor Manuel Gonzalez Morales
  */
 
 #include <iostream>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
+//Metodo para leer el archivo
+void read() {
+
+    ifstream archivo("../Pacientes.txt"); //Cargar Archivo
+
+    //Revisar que esté
+    if (!archivo.is_open()) {
+        cout << "No se pude abrir el archivo" << endl;
+        return;
+    }
+
+    string line; //String para guardar el contenido
+
+    //Bucle para leer la línea e imprimirla
+    while (getline(archivo,line)) {
+        cout << line << endl;
+    }
+
+    archivo.close(); //Cerrar el Archivo
+
+}
+
+
 int main() {
+    read();
     return 0;
 }
